640770197ddfa1d14e10260457aeaabf0c33d3bb
Kalebu <isaackeinstein@gmail.com>
2021-03-27 00:12:11 +0300

    fixed config bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,81 +6,80 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteMetadata: {
-      siteTitle: "Kalebu Jordan",
-      siteTitleAlt: "Kalebu Jordan",
-      siteHeadline: "Kalebu Jordan",
-      siteDescription: "A blog to learn and execrise your python skills",
-      siteLanguage: "en",
-      author: "Kalebu Jordan"
+    siteTitle: "Kalebu Jordan",
+    siteTitleAlt: "Kalebu Jordan",
+    siteHeadline: "Kalebu Jordan",
+    siteDescription: "A blog to learn and execrise your python skills",
+    siteLanguage: "en",
+    author: "Kalebu Jordan"
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/j_kalebu`,
+          },
+          {
+            name: `Instagram`,
+            url: `https://www.instagram.com/kalebu_jordan/`,
+          },
+        ],
+      },
     },
-    plugins: [
-      {
-        resolve: `@lekoarts/gatsby-theme-minimal-blog`,
-        // See the theme's README for all available options
-        options: {
-          navigation: [
-            {
-              title: `Blog`,
-              slug: `/blog`,
-            },
-            {
-              title: `About`,
-              slug: `/about`,
-            },
-          ],
-          externalLinks: [
-            {
-              name: `Twitter`,
-              url: `https://twitter.com/j_kalebu`,
-            },
-            {
-              name: `Instagram`,
-              url: `https://www.instagram.com/kalebu_jordan/`,
-            },
-          ],
-        },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
       },
-      {
-        resolve: `gatsby-plugin-google-analytics`,
-        options: {
-          trackingId: process.env.GOOGLE_ANALYTICS_ID,
-        },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+        short_name: `minimal-blog`,
+        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
       },
-      `gatsby-plugin-sitemap`,
-      {
-        resolve: `gatsby-plugin-manifest`,
-        options: {
-          name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-          short_name: `minimal-blog`,
-          description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
-          start_url: `/`,
-          background_color: `#fff`,
-          theme_color: `#6B46C1`,
-          display: `standalone`,
-          icons: [
-            {
-              src: `/android-chrome-192x192.png`,
-              sizes: `192x192`,
-              type: `image/png`,
-            },
-            {
-              src: `/android-chrome-512x512.png`,
-              sizes: `512x512`,
-              type: `image/png`,
-            },
-          ],
-        },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
       },
-      `gatsby-plugin-offline`,
-      `gatsby-plugin-netlify`,
-      shouldAnalyseBundle && {
-        resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
-        options: {
-          analyzerMode: `static`,
-          reportFilename: `_bundle.html`,
-          openAnalyzer: false,
-        },
-      },
-    ].filter(Boolean),
-  }
+    },
+  ].filter(Boolean),
+}
